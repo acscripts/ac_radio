77208fd0fa08b1e2ad56bff678a62c5210f80491
ANTOND <71350868+antond15@users.noreply.github.com>
2022-04-29 19:00:39 +0200

    refactor(server): Change framework checking logic

--- resource/server/server.lua
+++ resource/server/server.lua
@@ -1,17 +1,20 @@
+local function hasResource(name)
+	return GetResourceState(name):find('start')
+end
+
+local framework = (hasResource('es_extended') and 'esx') or (hasResource('qb-core') and 'qb') or (hasResource('ox_core') and 'ox') or ''
+
+-- Create usable items
 if not ac.useCommand then
 	CreateThread(function()
-		local function hasResource(name)
-			return GetResourceState(name):find('start')
-		end
-
 		if hasResource('ox_inventory') then
 			return
-		elseif hasResource('es_extended') then
+		elseif framework == 'esx' then
 			local ESX = exports.es_extended:getSharedObject()
 			ESX.RegisterUsableItem('radio', function(playerId)
 				TriggerClientEvent('ac_radio:openRadio', playerId)
 			end)
-		elseif hasResource('qb-core') then
+		elseif framework == 'qb' then
 			local QBCore = exports['qb-core']:GetCoreObject()
 			QBCore.Functions.CreateUseableItem('radio', function(playerId)
 				TriggerClientEvent('ac_radio:openRadio', playerId)
