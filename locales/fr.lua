0225c753b147d75f7a6ca63f0253da2598d42d6c
Ju <50602425+jumalley@users.noreply.github.com>
2022-06-28 21:58:41 +0200

    Update and rename en.lua to fr.lua
    french lang

---
+++ locales/fr.lua
@@ -0,0 +1,31 @@
+return {
+	['ui_join'] = "Rejoindre",
+	['ui_leave'] = 'Éteindre',
+	['ui_preset_set'] = "Predéfini",
+	['ui_preset_one'] = "Predéfini#1",
+	['ui_preset_two'] = "Predéfini#2",
+	['ui_volume_mute'] = "Muet",
+	['ui_volume_up'] = "Volume +",
+	['ui_volume_down'] = "Volume -",
+
+	['command_open'] = "Utiliser la radio",
+	['command_clear'] = "Supprimer les fréquences prédéfinies",
+	['keymap_open'] = "Utiliser la radio",
+
+	['channel_join'] = "Vous avez rejoint le canal %s MHz.",
+	['channel_unavailable'] = "Fréquence indisponible.",
+	['channel_disconnect'] = "Vous avez été déconnecté de fréquence.",
+
+	['volume_mute'] = "Radio mute.",
+	['volume_unmute'] = "Radio dé-mute.",
+	['volume_up'] = "Volume + %s%%",
+	['volume_down'] = "Volume - %s%%",
+	['volume_max'] = "Volume maximum.",
+	['volume_min'] = "volume minimum.",
+
+	['preset_not_found'] = "Aucune fréquence prédéfinie.",
+	['preset_choose'] = "Choisissez prédéfini (#1 ou #2) pour sauvegarder votre fréquence.",
+	['preset_invalid'] = "Fréquence invalide.",
+	['preset_set'] = "Fréquennce prédéfinie %s MHz",
+	['preset_clear'] = "Fréquences prédéfinies supprimées.",
+}
