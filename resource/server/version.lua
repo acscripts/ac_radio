8f5babc8f14597ba6a5ecb7574d2f91d7af02008
ANTOND <71350868+antond15@users.noreply.github.com>
2022-10-08 22:05:02 +0200

    fix(server/version): Properly check version
    applied fixes from https://github.com/overextended/ox_lib/commit/e624aa255996a658ba3a80496bdda1e818a6ffde

--- resource/server/version.lua
+++ resource/server/version.lua
@@ -1,23 +1,34 @@
--- Yoinked from https://github.com/overextended/ox_lib/blob/48728f0241da56b15f129fd4bcf394ccb01a95b9/resource/version/server.lua#L1
+-- Yoinked from https://github.com/overextended/ox_lib/blob/master/resource/version/server.lua
 if ac.versionCheck then
-	SetTimeout(1000, function()
-		local resource = GetCurrentResourceName()
+	local currentVersion = GetResourceMetadata('ac_radio', 'version', 0)
 
+	if currentVersion then
+		currentVersion = currentVersion:match('%d%.%d+%.%d+')
+	end
+
+	SetTimeout(1000, function()
 		PerformHttpRequest('https://api.github.com/repos/antond15/ac_radio/releases/latest', function(status, response)
 			if status ~= 200 then return end
 
 			response = json.decode(response)
 			if response.prerelease then return end
 
-			local currentVersion = GetResourceMetadata(resource, 'version', 0):match('%d%.%d+%.%d+')
-			if not currentVersion then return end
-
 			local latestVersion = response.tag_name:match('%d%.%d+%.%d+')
-			if not latestVersion then return end
+			if not latestVersion or latestVersion == currentVersion then return end
 
-			if currentVersion >= latestVersion then return end
+            local cv = { string.strsplit('.', currentVersion) }
+            local lv = { string.strsplit('.', latestVersion) }
 
-			print(('^3An update is available for ac_radio (current version: %s)\r\n%s^0'):format(currentVersion, response.html_url))
+            for i = 1, #cv do
+                local current, minimum = tonumber(cv[i]), tonumber(lv[i])
+
+                if current ~= minimum then
+                    if current < minimum then
+						return print(('^3An update is available for ac_radio (current version: %s)\r\n%s^0'):format(currentVersion, response.html_url))
+                    else break end
+                end
+            end
+
 		end, 'GET')
 	end)
 end
