c11df45a1de814a804bce643764d97f1532448a8
Nuga <93000491+nuga-dev@users.noreply.github.com>
2023-02-15 16:59:03 +0200

    feat(locales): estonian translation

---
+++ locales/et.lua
@@ -0,0 +1,31 @@
+return {
+	['ui_join'] = "Ühine sagedusega",
+	['ui_leave'] = 'Lülita välja',
+	['ui_preset_set'] = "Määra eelseade",
+	['ui_preset_one'] = "Eelseadistus #1",
+	['ui_preset_two'] = "Eelseadistus #2",
+	['ui_volume_mute'] = "Vaigista",
+	['ui_volume_up'] = "Valjemaks",
+	['ui_volume_down'] = "Vaiksemaks",
+
+	['command_open'] = "Avab raadio",
+	['command_clear'] = "Kustutab kõik raadio eelseadistused",
+	['keymap_open'] = "Ava raadio",
+
+	['channel_join'] = "Ühinesid sagedusega %s MHz",
+	['channel_unavailable'] = "See sagedus pole kättesaadav",
+	['channel_disconnect'] = "Katkestasid ühenduse raadioga",
+
+	['volume_mute'] = "Vaigistus peal",
+	['volume_unmute'] = "Vaigistus maas",
+	['volume_up'] = "Uus helitugevus: %s%%",
+	['volume_down'] = "Uus helitugevus: %s%%",
+	['volume_max'] = "Rohkem valjemaks ei saa enam panna",
+	['volume_min'] = "Vähem valjemaks ei saa enam panna",
+
+	['preset_not_found'] = "Ühtegi eelseadistust ei leitud",
+	['preset_choose'] = "Vali eelseadistus (1 või 2) et salvestada praegune sagedus.",
+	['preset_invalid'] = "Sobimatu sagedus",
+	['preset_set'] = "Määrasid eelseadistuse sageduseks %s MHz",
+	['preset_clear'] = "Raadio eelseadistused kustutatud",
+}
