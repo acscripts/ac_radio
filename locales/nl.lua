2aade4ddf9f99ff92331867c6392ee4abb4d1d4e
KevinL852 <93476027+KevinL852@users.noreply.github.com>
2024-01-12 17:20:37 +0100

    feat(locales): Dutch translation

---
+++ locales/nl.lua
@@ -0,0 +1,31 @@
+return {
+	['ui_join'] = "Deelnemen aan frequentie",
+	['ui_leave'] = 'Uitschakelen',
+	['ui_preset_set'] = "Preset instellen",
+	['ui_preset_one'] = "Preset #1",
+	['ui_preset_two'] = "Preset #2",
+	['ui_volume_mute'] = "Dempen",
+	['ui_volume_up'] = "Volume omhoog",
+	['ui_volume_down'] = "Volume omlaag",
+
+	['command_open'] = "Opent de radio UI",
+	['command_clear'] = "Wis alle frequentie presets",
+	['keymap_open'] = "Open de radio UI",
+
+	['channel_join'] = "Gekoppeld aan frequentie %s MHz",
+	['channel_unavailable'] = "Deze frequentie is niet beschikbaar",
+	['channel_disconnect'] = "Je hebt de verbinding verbroken",
+
+	['volume_mute'] = "Radio gedempt",
+	['volume_unmute'] = "Radio niet meer gedempt",
+	['volume_up'] = "Volume verhoogd naar %s%%",
+	['volume_down'] = "Volume verlaagd naar %s%%",
+	['volume_max'] = "Maximaal volume bereikt",
+	['volume_min'] = "Minimaal volume bereikt",
+
+	['preset_not_found'] = "Geen presets gevonden",
+	['preset_choose'] = "Kies een preset (1 of 2) om de huidige frequentie op te slaan.",
+	['preset_invalid'] = "Geen geldige fequentie",
+	['preset_set'] = "U heeft de preset ingesteld op %s MHz",
+	['preset_clear'] = "Radio presets verwijdert",
+}
