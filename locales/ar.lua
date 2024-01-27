a190c020272e484d6f13e4d63d33c36e02e62927
Achraf <136148902+achrafvod@users.noreply.github.com>
2024-01-27 12:07:54 +0100

    Create ar.lua

---
+++ locales/ar.lua
@@ -0,0 +1,31 @@
+return {
+	['ui_join'] = "الإنضمام لتردد",
+	['ui_leave'] = 'إغلاق',
+	['ui_preset_set'] = "تحديد مثبث سابقا",
+	['ui_preset_one'] = "المثبث #1",
+	['ui_preset_two'] = "المثبث #2",
+	['ui_volume_mute'] = "كتم",
+	['ui_volume_up'] = "زيادة الصوت",
+	['ui_volume_down'] = "خفض الصوت",
+
+	['command_open'] = "فتح الراديو",
+	['command_clear'] = "مسح إعدادات الراديو المسبقة",
+	['keymap_open'] = "Open radio UI",
+
+	['channel_join'] = "إتصلت بالتردد %s MHz",
+	['channel_unavailable'] = "لا يوجد هدا التردد",
+	['channel_disconnect'] = "تم قطع الإتصال بالراديو",
+
+	['volume_mute'] = "الراديو مكتوم",
+	['volume_unmute'] = "الراديو مفتوح",
+	['volume_up'] = "زيادة الصوت لـ %s%%",
+	['volume_down'] = "خفض الصوت لـ %s%%",
+	['volume_max'] = "تم الوصول إلى الحد الأقصى",
+	['volume_min'] = "تم الوصول إلى الحد الأدنى",
+
+	['preset_not_found'] = "لم يتم العثور على أي إعداد مثبث من قبل",
+	['preset_choose'] = "إختر المثبث (1 أو 2) لحفظ التردد الحالي",
+	['preset_invalid'] = "تردد غير صالح",
+	['preset_set'] = "لقد قمت بضبط الإعداد المسبق على %s MHz",
+	['preset_clear'] = "تم مسح إعدادات الراديو المسبقة",
+}
