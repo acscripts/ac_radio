94e4c543dcd1866e6db11ae4f49a0028e14b84ce
ANTOND. <71350868+antond15@users.noreply.github.com>
2024-05-06 23:45:42 +0200

    feat(config): explanation

--- config.lua
+++ config.lua
@@ -1,11 +1,30 @@
-return {
+-----------------------------------------------------------------
+-- Visit https://docs.acscripts.dev/radio for documentation
+-----------------------------------------------------------------
 
+return {
+    -- Enable usable item for opening the radio
+    useUsableItem = true,
+
+    -- Enable command for opening the radio
+    useCommand = false,
+
+    -- Default keybind for the '/radio' command
+    commandKey = '',
+
+    -- Enable disconnecting from frequency when there is no radio item left in player's inventory
+    disconnectWithoutRadio = true,
+
+    -- Percentage of volume to increase/decrease per step
     volumeStep = 10,
 
+    -- Frequency decimal precision
     frequencyStep = 0.01,
 
+    -- Maximum amount of available frequencies (starting from 0)
     maximumFrequencies = 1000,
 
+    -- Frequency restrictions for channels
     restrictedChannels = {
         [10] = 'police',
         [11] = {
@@ -13,13 +32,13 @@ return {
         },
     },
 
-    disconnectWithoutRadio = true,
-
-    useUsableItem = true,
-    useCommand = true,
-    commandKey = '',
-
+    -- ! The following options will override pma-voice convars
+    -- Enable radio voice effect (voice sounds like on a real radio)
     radioEffect = true,
+
+    -- Enable animation while talking on radio
     radioAnimation = true,
+
+    -- Default keybind for talking on radio
     radioTalkKey = 'LMENU',
 }
