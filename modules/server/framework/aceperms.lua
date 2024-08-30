8dea3320f666d4c96b24a7201999f2041723993b
FearlessNite345 <110247392+FearlessNite345@users.noreply.github.com>
2024-08-29 20:07:15 -0400

    Fix ace perms compatibility with restricted GRADE type channels

--- modules/server/framework/aceperms.lua
+++ modules/server/framework/aceperms.lua
@@ -4,23 +4,23 @@ local Config = require 'config'
 Citizen.CreateThread(function()
     for frequency, allowed in pairs(Config.restrictedChannels) do
         Voice:addChannelCheck(tonumber(frequency), function(playerId)
-            if type(allowed) == 'table' then
-                for _, ace in pairs(allowed) do
-                    if IsPlayerAceAllowed(playerId, ace) then
-                        lib.notify(playerId, {
-                            type = 'success',
-                            description = locale('channel_join', frequency + 0.0),
-                        })
-                        return true
-                    end
-                end
-            else
+            if type(allowed) == 'string' then
                 if IsPlayerAceAllowed(playerId, allowed) then
                     lib.notify(playerId, {
                         type = 'success',
                         description = locale('channel_join', frequency + 0.0),
                     })
                     return true
+                end
+            elseif type(allowed) == 'table' then
+                for _, ace in pairs(allowed) do
+                    if type(ace) == 'string' and IsPlayerAceAllowed(playerId, ace) then
+                        lib.notify(playerId, {
+                            type = 'success',
+                            description = locale('channel_join', frequency + 0.0),
+                        })
+                        return true
+                    end
                 end
             end
 
